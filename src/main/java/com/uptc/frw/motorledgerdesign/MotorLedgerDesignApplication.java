70563fbb8d76c5be9b508319a041ad7b1337323b
semilla <semilla@semilla.com>
2025-10-15 17:33:55 -0500

    Initial commit

---
+++ src/main/java/com/uptc/frw/motorledgerdesign/MotorLedgerDesignApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.motorledgerdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorLedgerDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorLedgerDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/motorledgerdesign/MotorLedgerDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.motorledgerdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorLedgerDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
